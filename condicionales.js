0e30a00049c2b4164a417b0e75ba64d8fd3f41de
Jesse Ilescas <ilesmj9516@gmail.com>
2022-03-22 17:56:21 -0600

    Creando repositorio

---
+++ condicionales.js
@@ -0,0 +1,11 @@
+console.log('Clase de condicionales (If)')
+let variable = true
+
+if (variable){
+    console.log('Saludos Jesse')
+}
+
+let numero = 25
+if(numero ==25){
+    console.log('El numero es 25')
+}
